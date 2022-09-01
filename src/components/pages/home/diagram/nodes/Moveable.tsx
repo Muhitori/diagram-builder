d544f3b12eb440b8bf2d0e07d8ce966f5f61ab11
muhitori <turevichkolya@gmail.com>
2022-09-02 00:21:12 +0300

    moveable component

---
+++ src/components/pages/home/diagram/nodes/Moveable.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import MoveableComponent, { OnDrag, OnResize, OnRotate, OnScale } from 'react-moveable';
+
+interface Props {
+  node: HTMLElement;
+}
+
+export const Moveable: FC<Props> = ({ node }) => {
+  return (
+    <MoveableComponent
+      target={node}
+      container={null}
+      origin={true}
+      edge={false}
+      draggable={true}
+      throttleDrag={0}
+      onDrag={({ target, left, top }: OnDrag) => {
+        target.style.left = `${left}px`;
+        target.style.top = `${top}px`;
+      }}
+      keepRatio={true}
+      resizable={true}
+      throttleResize={0}
+      onResize={({ target, width, height }: OnResize) => {
+        target.style.width = `${width}px`;
+        target.style.height = `${height}px`;
+      }}
+      scalable={true}
+      throttleScale={0}
+      onScale={({ target, transform }: OnScale) => {
+        target.style.transform = transform;
+      }}
+      /* rotatable */
+      rotatable={true}
+      throttleRotate={0}
+      onRotate={({ target, transform }: OnRotate) => {
+        target.style.transform = transform;
+      }}
+    />
+  );
+};
