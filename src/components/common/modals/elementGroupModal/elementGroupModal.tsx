23eb06c602b97adc8e5adde0b46f77497b3768a2
muhitori <turevichkolya@gmail.com>
2022-09-15 15:14:45 +0300

    node modal & element group modal

---
+++ src/components/common/modals/elementGroupModal/elementGroupModal.tsx
@@ -0,0 +1,80 @@
+import { FormikProps } from 'formik';
+import { FC, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { snackbarGenerator } from 'src/components/SnackbarGenerator';
+import { setGroupGeneralOptions } from 'src/store/slice';
+import { ElementFormData, NodeFormData } from 'src/types/Forms';
+import { Dialog } from '../../dialog/Dialog';
+import { Form } from '../../form/Form';
+
+interface Props {
+  groupName: string;
+  title: string;
+  open: boolean;
+  onClose: () => void;
+  submitButtonName: string;
+  values?: NodeFormData;
+}
+
+const fields = [
+  { name: 'name', label: 'Node name', fullWidth: true },
+  { name: 'color', type: 'color', label: 'Color:' },
+];
+
+const initialValues = {
+  name: '',
+  color: '#ffffff',
+};
+
+export const ElementGroupModal: FC<Props> = ({
+  groupName,
+  title,
+  open,
+  onClose,
+  submitButtonName,
+  values,
+}) => {
+  const dispatch = useDispatch();
+  const formRef = useRef<FormikProps<ElementFormData>>(null);
+
+  const handleDialogSubmit = () => {};
+
+  const handleFormSubmit = (data: NodeFormData) => {
+    const { name: prefix, color } = data;
+    const elementPrefix = prefix.trim();
+
+    if (groupName) {
+      dispatch(
+        setGroupGeneralOptions({
+          groupName,
+          generalOptions: {
+            name: elementPrefix,
+            color,
+          },
+        })
+      );
+      snackbarGenerator.success(`${groupName} updated.`);
+      onClose();
+    } else {
+      snackbarGenerator.error('Error while group updating.');
+    }
+  };
+
+  return (
+    <Dialog
+      title={title}
+      open={open}
+      onClose={onClose}
+      onSubmit={handleDialogSubmit}
+      size="sm"
+      submitButtonName={submitButtonName}
+    >
+      <Form
+        formRef={formRef}
+        initialValues={values || initialValues}
+        onSubmit={handleFormSubmit}
+        fields={fields}
+      />
+    </Dialog>
+  );
+};
